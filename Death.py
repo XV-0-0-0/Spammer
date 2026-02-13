aceb8f48ae2f432b5996d852641aef3d94118d6f
Владислав Хорошун <vlad.X.B2024@outlook.com>
2026-02-13 22:09:29 +0100

    2.0

--- Death.py
+++ Death.py
@@ -1,40 +1,49 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, errors
 import asyncio
 
 api_id =
+
 api_hash = ''
 
 friend_username = ''
 message_text = ""
 
-REQUESTS = 3
-CONCURRENCY = 1
+REQUESTS = 500
 
-YOUR_PASSWORD = ''
-
-
-async def worker(client, i):
-    try:
-        await client.send_message(friend_username, message_text)
-        print(f"Message {i}: Successfully sent")
-    except Exception as e:
-        print(f"Message {i}: Error: {e}")
-
+YOUR_PASSWORD = '1234567890qwe@#*'
 
 async def run_test():
-
     client = TelegramClient('my_birthday_session', api_id, api_hash)
-
     await client.start(password=YOUR_PASSWORD)
 
-    print("Authorisation successful! Starting to send...")
+    print(f" I am starting a marathon on {REQUESTS} Message")
 
-    tasks = [worker(client, i) for i in range(REQUESTS)]
-    await asyncio.gather(*tasks)
+    sent_count = 0
 
-    print("\n---- DONE ----")
+    while sent_count < REQUESTS:
+        try:
+
+            text = message_text.format(sent_count + 1)
+
+            await client.send_message(friend_username, text)
+            sent_count += 1
+
+            print(f"[{sent_count}/{REQUESTS}] successful")
+
+            await asyncio.sleep(0.3)
+
+        except errors.FloodWaitError as e:
+            print(f"\n Telegram FloodWait: one must wait {e.seconds} seconds.")
+            print("Waiting")
+            await asyncio.sleep(e.seconds + 5)
+            print("continue\n")
+
+        except Exception as e:
+            print(f" Another mistake: {e}")
+            await asyncio.sleep(2)
+
+    print("\nAll messages sent.")
     await client.disconnect()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(run_test())
